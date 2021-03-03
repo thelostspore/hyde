e6bf59cea20c90ef22006e6621fd29c855e1ad0a
thelostspore <thelostspore@gmail.com>
2021-03-02 21:53:01 -0800

    Customize directory structure, update Next.JS v10

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    target: 'serverless',
-    webpack: function (config) {
-        config.module.rules.push({test:  /\.md$/, use: 'raw-loader'})
-        config.module.rules.push({test: /\.yml$/, use: 'raw-loader'})
-        return config
-    },
+  webpack: function (config) {
+      config.module.rules.push({test: /\.md$/, use: 'raw-loader'})
+      config.module.rules.push({test: /\.yml$/, use: 'raw-loader'})
+      return config
   }
+}
